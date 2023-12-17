38f19b6628c18a58cacc2a35cb55add8e5c8806b
thianecw <thianecw@hotmail.com>
2023-12-17 20:40:28 +0000

    novo alerta

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ function generatenumber(){
 const min = Math.ceil(document.querySelector(".input-min").value)
 const max = Math.floor(document.querySelector(".input-max").value)
 
-const result = Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
-
+if( min >= max ) {
+    alert("O valor mínimo tem que ser MENOR que o valor máximo")
+} else {
+    const result = Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+    
 alert(result)
-}
 
+}
+}
